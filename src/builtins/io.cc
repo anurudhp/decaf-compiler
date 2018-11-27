d4968b511cb65152a595af9d361b3794db953a1b
Anurudh Reddy <anurudh.peduri@research.iiit.ac.in>
2018-11-27 07:08:19 +0530

    [builtins] Fix bugs, add write_bool

--- src/builtins/io.cc
+++ src/builtins/io.cc
@@ -1,18 +1,34 @@
 #include <iostream>
 
-// read_int: reads one integer from stdin
-int read_int() {
-	int val;
-	std::cin >> val;
-	return val;
-}
+extern "C" {
+	// read_int: reads one integer from stdin
+	int read_int() {
+		int val;
+		std::cin >> val;
+		return val;
+	}
 
-// write_int: prints one integer to stdout
-void write_int(int val) {
-	std::cout << val;
-}
+	// write_int: prints one integer to stdout
+	int write_int(int val) {
+		std::cout << val;
+		return 0;
+	}
 
-// write_char: prints one character to stdout
-void write_char(char val) {
-	std::cout << val;
+	// write_bool: prints one boolean value to stdout
+	int write_bool(bool val) {
+		std::cout << std::boolalpha << val << std::noboolalpha;
+		return 0;
+	}
+
+	// write_char: prints one character to stdout
+	int write_char(char val) {
+		std::cout << val;
+		return 0;
+	}
+
+	// write_string: prints a string literal to stdout
+	int write_string(const char* val) {
+		std::cout << val;
+		return 0;
+	}
 }
