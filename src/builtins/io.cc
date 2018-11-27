65b9d777d488c693b16fbe31e9ce9f1b3289abce
Anurudh Reddy <anurudh.peduri@research.iiit.ac.in>
2018-11-27 22:55:38 +0530

    [test-programs] Add segment-tree

--- src/builtins/io.cc
+++ src/builtins/io.cc
@@ -8,27 +8,34 @@ extern "C" {
 		return val;
 	}
 
+	// read_char: reads one character from stdin
+	int read_char() {
+		char val;
+		std::cin >> val;
+		return val;
+	}
+
 	// write_int: prints one integer to stdout
 	int write_int(int val) {
-		std::cerr << val;
+		std::cout << val;
 		return 0;
 	}
 
 	// write_bool: prints one boolean value to stdout
 	int write_bool(bool val) {
-		std::cerr << std::boolalpha << val << std::noboolalpha;
+		std::cout << std::boolalpha << val << std::noboolalpha;
 		return 0;
 	}
 
 	// write_char: prints one character to stdout
 	int write_char(char val) {
-		std::cerr << val;
+		std::cout << val;
 		return 0;
 	}
 
 	// write_string: prints a string literal to stdout
 	int write_string(const char* val) {
-		std::cerr << val;
+		std::cout << val;
 		return 0;
 	}
 }
