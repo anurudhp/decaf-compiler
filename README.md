e688f48ff9294d95bd1fa160b370973d0aff1e61
Anurudh Reddy <anurudh.peduri@research.iiit.ac.in>
2018-11-27 21:06:31 +0530

    Update readme

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ CSE419 Compilers (Monsoon 2018)
 ### Phases
 - [x] Phase 1: Grammar
 - [x] Phase 2: AST construction, basic semantic rules
-- [ ] Phase 3: LLVM IR generation
+- [x] Phase 3: LLVM IR generation
 - [x] optional: Semantic Analysis (full)
+
+### Usage
+- Build decaf: `make clean && make`
+- compiling code: `bin/compile <path/to/code> [clang-opts]`
+	- Sample usage: `bin/compile test-programs/arraysum.dcf -o a.out -O2`
--- src/compile.sh
+++ src/compile.sh
@@ -1,26 +1,18 @@
 #! env bash
 
-# Wrapper script to compile IR to object code
-# install into bin
+# Wrapper script to compile IR to binary
+# install script to `bin`
 
 # @arg $1 : path/to/code.dcf
 # @arg $2 opt : path/to/executable, defaults to ./a.out
 
 if [[ "$#" -lt 1 ]] ; then
-	echo "Usage: bin/compile <path/to/code.dcf> [<path/to/executable>] [clang-opts]"
+	echo "Usage: bin/compile <path/to/code.dcf> [clang-opts]"
 	exit
 fi
 
 code=$1
-
-if [[ "$#" -lt 2 ]] ; then
-	out=a.out
-else
-	out=$2
-fi
-
-shift
-shift
-
 ./bin/decaf $code --output=bin/.temp.ll
-clang++ $@ -Wno-override-module build/builtins.o bin/.temp.ll -o $out
+
+shift
+clang++ $@ -Wno-override-module build/builtins.o bin/.temp.ll
